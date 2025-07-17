ce0035c15f11e1a987a9dc8be6c5667ad96bdcbf
AhZanMC <151012474+AhZanMC@users.noreply.github.com>
2025-07-17 13:59:02 +0700

    Initial commit

---
+++ application/views/about.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Page</title>
+</head>
+<body>
+    <h1>Ini halaman About</h1>
+    <p>Selamat datang di halaman about!</p>
+</body>
+</html>
---
+++ application/views/contact.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Page</title>
+</head>
+<body>
+    <h1>Ini halaman Contact</h1>
+    <p>Hubungi kami di sini!</p>
+</body>
+</html>
